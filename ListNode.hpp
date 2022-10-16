df34bb0f52f14cf44c47c2abe66f989bd555e6b5
ZhuStudent <115903346+ZhuStudent@users.noreply.github.com>
2022-10-16 08:44:36 -0400

    ListNode.hpp

---
+++ ListNode.hpp
@@ -0,0 +1,22 @@
+#ifndef ListNode_hpp
+#define ListNode_hpp
+
+class StudentList
+{
+    private:
+    struct ListNode
+    {
+        int id;
+        double gpa;
+        struct ListNode *next;
+    };
+    ListNode *head;
+    
+    public:
+    StudentList();
+    void appendNode(int, double);
+    void insertNode(int, double);
+    void deleteNode(int, double);
+    void displayList() const;
+};
+#endif
